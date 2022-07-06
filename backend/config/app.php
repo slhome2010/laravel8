fc8af25891b49b434e3c979eedb7dd93597c2ce7
slhome2010 <slhome2010@mail.ru>
2022-07-06 15:21:23 +0600

    Fix Migrations error

--- backend/config/app.php
+++ backend/config/app.php
@@ -56,6 +56,8 @@ return [
 
     'frontend_url' => env('FRONTEND_URL', 'http://localhost:3000'),
 
+    'frontend_url' => env('FRONTEND_URL', 'http://localhost:3000'),
+
     'asset_url' => env('ASSET_URL', null),
 
     /*
--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "269979b179e19892"; }
+/******/ 		__webpack_require__.h = function() { return "8a22bf8f5f41524d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
